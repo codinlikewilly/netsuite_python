6a22843052d0b726f3a3b3dd975f85ddcf5b6554
will <will@theapiguys.com>
2023-02-01 12:53:11 -0500

    fix RestClient

--- netsuite/swagger_client/rest_client.py
+++ netsuite/swagger_client/rest_client.py
@@ -1,14 +1,13 @@
-
-from netsuite.swagger_client.configuration import Configuration
 from netsuite import swagger_client
-from netsuite.swagger_client.api import contact_api, customer_api
 
 
 class RestClient:
     def __init__(self, netsuite):
         self.netsuite = netsuite
-        self.configuration = Configuration()
+        self.configuration = swagger_client.Configuration()
         self.configuration.app_name = netsuite.netsuite_app_name
-        self.configuration.access_token = netsuite.storage.get_access_token(netsuite.app_name)
-        self.contact_api = contact_api.ContactApi(swagger_client.ApiClient(self.configuration))
-        self.customer_api = customer_api.CustomerApi(swagger_client.ApiClient(self.configuration))
+        self.configuration.host = f"https://{self.configuration.app_name}.suitetalk.api.netsuite.com/services/rest/record/v1"
+        self.configuration.access_token = netsuite.storage.get_token(netsuite.app_name).access_token
+        self.api_client = swagger_client.ApiClient(configuration=self.configuration)
+        self.contact_api = swagger_client.ContactApi(api_client=self.api_client)
+        self.customer_api = swagger_client.CustomerApi(api_client=self.api_client)
