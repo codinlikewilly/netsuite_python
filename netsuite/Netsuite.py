c49ad3da3d81dc5b973a1319b5dfd96d607ae6b2
will <will@theapiguys.com>
2025-03-04 12:17:07 -0500

    update encoding algorith in jwt.encode in get_jwt() to PS256

--- netsuite/Netsuite.py
+++ netsuite/Netsuite.py
@@ -8,6 +8,7 @@ from netsuite.NetsuiteToken import NetsuiteToken
 from netsuite.swagger_client.rest_client import RestClient, QueryClient
 from netsuite.settings import APISettings
 from netsuite.storages import BaseStorage, JSONStorage
+from netsuite import api_clients
 
 
 class Netsuite:
@@ -133,6 +134,117 @@ class Netsuite:
             return self.token
         else:
             return self.request_access_token()
+
+    def get_netsuite_recordtypes(self):
+        url = f"https://{self.netsuite_app_name}.suitetalk.api.netsuite.com/services/rest/record/v1/metadata-catalog"
+        token = self.storage.get_token(self.app_name)
+        headers = {
+            'Authorization': f'Bearer {token.access_token}'
+        }
+        response = requests.get(url, headers=headers)
+        records = []
+        for item in response.json().get('items'):
+            records.append(item.get("name"))
+        return sorted(records)
+
+    def generate_rest_client(self, record_types=None):
+
+        print("\nGenerating Client....")
+
+        # from urllib.request import urlopen
+        # from tempfile import NamedTemporaryFile
+        # from shutil import unpack_archive
+        import zipfile, shutil
+        from io import BytesIO
+        token = self.storage.get_token(self.app_name)
+        url = f"https://{self.netsuite_app_name}.suitetalk.api.netsuite.com/services/rest/record/v1/metadata-catalog"
+        params = {
+            'select': record_types
+        }
+        headers = {
+            'Accept': 'application/swagger+json',
+            'Authorization': f'Bearer {token.access_token}'
+        }
+        response = requests.get(url, headers=headers, params=params)
+        # print(token.access_token)
+        # print(response.json())
+        data = {
+            'options': {
+                'packageName': 'netsuite_rest_client'
+            },
+            'generateSourceCodeOnly': 'True',
+            'spec': response.json()
+        }
+        headers2 = {
+            'Content-Type': 'application/json'
+        }
+        result = requests.post('https://api-latest-master.openapi-generator.tech/api/gen/clients/python',headers=headers2, json=data)
+
+        # Defining the zip file URL
+        url = result.json().get('link')
+
+        # Split URL to get the file name
+        filename = url.split('/')[-1]
+
+        # Downloading the file by sending the request to the URL
+        print('Downloading Generated Client....')
+        req = requests.get(url)
+        print('Client Downloaded.')
+
+        # extracting the zip file contents
+        print('Extracting....')
+        file = zipfile.ZipFile(BytesIO(req.content))
+        file.extractall(path=self.api_settings.NETSUITE_CLIENT_PATH)
+
+
+        client_src = Path.joinpath(Path(self.api_settings.NETSUITE_CLIENT_PATH), f'python-client/netsuite_rest_client')
+        class_src = Path.joinpath(Path(netsuite.settings.PACKAGE_DIR), f'netsuite_rest_client/rest_api_client.py')
+        # docs_src = Path.joinpath(Path(self.api_settings.NETSUITE_CLIENT_PATH), f'python-client/docs')
+        readme_src = Path.joinpath(Path(self.api_settings.NETSUITE_CLIENT_PATH), f'python-client/README.md')
+
+        dst = Path(f'{self.api_settings.NETSUITE_CLIENT_PATH}')
+        # docs_dst = Path.joinpath(Path(self.api_settings.NETSUITE_CLIENT_PATH), f'docs')
+        print(f'Copying to: {dst}')
+        shutil.copytree(client_src, dst, symlinks=True, ignore=None, ignore_dangling_symlinks=False,
+                        dirs_exist_ok=True)
+        # shutil.copytree(docs_src, docs_dst, symlinks=True, ignore=None, ignore_dangling_symlinks=False,
+        #                 dirs_exist_ok=True)
+
+        shutil.copy(class_src, dst)
+        shutil.copy(readme_src, dst)
+
+
+        shutil.rmtree(Path.joinpath(Path(self.api_settings.NETSUITE_CLIENT_PATH), 'python-client'))
+        print('temp folder was removed.')
+
+        print('\nNetsuite client was successfully generated and is ready to use!')
+        print('***************************************************************')
+        print('\nUSAGE')
+        print('----------------------')
+        print('from netsuite import Netsuite'
+              '\nfrom netsuite_rest_client import apis, rest_api_client'
+              '\nnetsuite = Netsuite()'
+              '\nrest_client = rest_api_client.RestApiClient(netsuite)'
+              '\ncustomer_api = apis.CustomerApi(rest_client)')
+
+    class RestletClient:
+        def __init__(self, netsuite):
+            self.netsuite = netsuite
+            self.configuration = api_clients.Configuration()
+            self.configuration.token = netsuite.storage.get_token(netsuite.app_name)
+            self.configuration.token_refresh_hook = self.refresh_token
+            self.configuration.app_name = netsuite.netsuite_app_name
+            self.configuration.host = f"https://{self.configuration.app_name}.restlets.api.netsuite.com/app/site/hosting/restlet.nl"
+            self.api_client = api_clients.ApiClient(configuration=self.configuration)
+            self.restlet_api = api_clients.RestletApi(api_client=self.api_client)
+
+            # self.contact_api = swagger_client.ContactApi(api_client=self.api_client)
+            # self.customer_api = swagger_client.CustomerApi(api_client=self.api_client)
+            # self.message_api = swagger_client.MessageApi(api_client=self.api_client)
+
+        def refresh_token(self):
+            self.configuration.token = self.netsuite.get_token()
+            return self.configuration.token
 
     # def get_status_dict(self):
     #     if self.token.access_token is None:
--- netsuite/scripts/cli.py
+++ netsuite/scripts/cli.py
@@ -66,8 +66,12 @@ def cli():
 #         f.write(dump_certificate(FILETYPE_PEM, cert))
 #     with open(NETSUITE_KEY_FILE, 'wb+') as f:
 #         f.write(dump_privatekey(FILETYPE_PEM, key))
+
 @cli.command()
 def generate_certificate():
+    generate_netsuite_certificate()
+
+def generate_netsuite_certificate():
     # Prompt for certificate attributes
     CN = click.prompt("Domain", hide_input=False)
     ORG = click.prompt("Organization", hide_input=False)
@@ -168,6 +172,78 @@ def generate_client_config():
 
     return creds
 
+@cli.command()
+def generate_netsuite_client_config():
+    try:
+        netsuite = Netsuite()
+        if netsuite.api_settings.CLIENT_ID is not None:
+            from pprint import pprint
+            print("Netsuite Credentials Found.")
+            print("     CURRENT CONFIG")
+            print("-------------------------")
+            pprint(netsuite.api_settings.__dict__.get('_user_settings'), indent=4)
+            print("\n")
+            if prompt("Keep settings?", type=click.BOOL, default=True):
+                return
+    except Exception:
+        print("")
+    print("****************************")
+    print("  GENERATE NETSUITE CONFIG")
+    print("****************************")
+    steps_completed = False
+    while not steps_completed:
+        steps_completed = prompt(f"Have you created the integration record by following the steps in the README?", default='y', type=click.BOOL, show_choices=True)
+        if steps_completed:
+            client_id = prompt("Client Id", hide_input=False)
+        else:
+            print('Read it and do the steps hoe https://bitbucket.org/theapiguys/netsuite_python/src/master/')
+
+    print("\n --- Certificate Configuration --- \n ")
+    certificate_uploaded = False
+    while not certificate_uploaded:
+        certificate_uploaded = prompt(f"Have you generated a certificate and uploaded it to Netsuite?", default='n', type=click.BOOL, show_choices=True)
+        if certificate_uploaded:
+            cert_id = prompt("Certificate ID: ", hide_input=False)
+        else:
+            generate_netsuite_certificate()
+
+    print("\n --- Certificate has been Configured --- \n ")
+    # client_secret = prompt("What is your client secret?", hide_input=True)
+    # redirect_url = prompt("Redirect URL", default=api_settings.REDIRECT_URL)
+    netsuite_app_name = prompt("What is the netsuite application name (Portion before app.netsuite.com)?", hide_input=False)
+    app_name = prompt("App Name (for token storage)", default=api_settings.APP_NAME)
+    # storage_class = prompt("Storage Class", default=api_settings.defaults.get('STORAGE_CLASS'),
+    #                        type=click.Choice(api_settings.defaults.get('DEFAULT_STORAGE_CLASSES')),
+    #                        show_choices=True)
+    storage_class = api_settings.defaults.get('STORAGE_CLASS')
+
+    creds = {
+        'CLIENT_ID': client_id,
+        'CERT_ID': cert_id,
+        # 'CLIENT_SECRET': client_secret,
+        # 'REDIRECT_URL': redirect_url,
+        'NETSUITE_APP_NAME': netsuite_app_name,
+        'APP_NAME': app_name,
+        'ALLOW_NONE': api_settings.ALLOW_NONE,
+        'USE_DATETIME': api_settings.USE_DATETIME,
+        'STORAGE_CLASS': storage_class,
+    }
+
+    if storage_class == JSON_STORAGE:
+        creds['JSON_STORAGE_PATH'] = prompt("Token Storage File", default=api_settings.JSON_STORAGE_PATH,
+                                            type=click.Path(readable=True, writable=True))
+
+
+    with open(api_settings.CREDENTIALS_PATH, 'w') as f:
+        creds_json = json.dumps(creds, indent=4)
+        f.write(creds_json)
+        print(f"Netsuite Credentials path: {api_settings.CREDENTIALS_PATH} ")
+    print("\n ----- Configuration Generated -----")
+
+
+
+    return creds
+
 
 @cli.command()
 @click.option('--credentials-file', '--f', type=click.File('r'), default=api_settings.CREDENTIALS_PATH,
@@ -201,6 +277,73 @@ def get_access_token(ctx, credentials_file):
                 click.echo(netsuite.token.__dict__)
             else:
                 click.echo(f"Saved to {credentials_file}")
+
+@cli.command()
+def generate_rest_client():
+    generate_netsuite_rest_client()
+
+def generate_netsuite_rest_client():
+    ns_records_to_include = []
+    print (" ----- Rest Client Generator -----")
+    netsuite = Netsuite()
+    use_record_wizard = prompt("Use Record Wizard?", type=click.BOOL, default=True)
+    if use_record_wizard:
+        needs_other_recordtypes = prompt("Will you be using record types other than customer?", type=click.BOOL, default=True)
+
+
+        if needs_other_recordtypes:
+
+            display_ns_classes = prompt("List available Netsuite Records?", type=click.BOOL, default=True)
+            print("")
+            print(" Getting Record Types... May take a minute...")
+            records = netsuite.get_netsuite_recordtypes()
+            if display_ns_classes:
+                display_custom = prompt("Display Custom records (probably not)?", type=click.BOOL, default=False)
+                print("\n   RECORDS")
+                print("-------------------")
+                if display_custom:
+                    for record in records:
+                        print(record)
+                else:
+                    for record in records:
+                        if "customrecord" not in record and "customlist" not in record:
+                            print(record)
+
+            print("\n")
+            add_more_records = True
+            while add_more_records:
+                next_record = prompt("Which records do you need?", default='')
+                if next_record == '':
+                    if prompt("Skip this step and use default (customer)?", type=click.BOOL, default=False):
+                        break
+                else:
+                    if next_record not in records:
+                        print("That record is not available.")
+                    elif next_record in ns_records_to_include:
+                        print("record already included.")
+                    else:
+                         ns_records_to_include.append(next_record)
+                         print(f"Record Added: {next_record}")
+                add_more_records = prompt("Add another?", type=click.BOOL, default='yes', show_choices=True)
+
+
+        if len(ns_records_to_include) >> 0:
+            record_str = ''
+            index = 0
+            for record in ns_records_to_include:
+                print(len(ns_records_to_include))
+                if index == 0:
+                    print('index was 0')
+                    record_str += f'{record}'
+                    index += 1
+                else:
+                    record_str += f',{record}'
+        else:
+            record_str = 'customer'
+    else:
+        record_str = prompt("Enter Record Types (comma separated)", default='customer')
+    print(f"Record STR: record_str")
+    netsuite.generate_rest_client(record_types=record_str)
 
 
 if __name__ == "__main__":
